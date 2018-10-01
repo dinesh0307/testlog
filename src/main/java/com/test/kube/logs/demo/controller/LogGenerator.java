90bc69f36d25523fbf9d4cf5a114f15cced85467
d5s <dinesh.5.s@nokia.com>
2018-10-01 14:55:31 +0530

    Test log for kube

---
+++ src/main/java/com/test/kube/logs/demo/controller/LogGenerator.java
@@ -0,0 +1,30 @@
+package com.test.kube.logs.demo.controller;
+
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping( "/generatelogs" )
+public class LogGenerator
+{
+
+    private Logger log = Logger.getLogger( LogGenerator.class );
+
+
+    @RequestMapping( method = RequestMethod.GET )
+    public @ResponseBody String sayHello()
+    {
+        for( int i = 0; i < 10; i++ )
+        {
+            log.info( "Generating logs for time : " + i );
+        }
+        return "Log generated : " + new Date();
+    }
+
+}
